df90bf5b44a10d95cc3b9961e38cbd4b3244ccba
Arne Olai Molland <arneolaim@gmail.com>
2018-11-03 15:28:24 +0100

    :tada: Init

---
+++ src/main/webapp/WEB-INF/startup.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS participants,participantlist;
+
+CREATE TABLE participantlist (
+    id INTEGER,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE participants (
+    gender CHAR,
+    name VARCHAR(20),
+    hashedpwd VARCHAR,
+    phone VARCHAR(8),
+    PARTICIPANTLIST_ID INTEGER,
+    PRIMARY KEY (phone),
+    CONSTRAINT fk FOREIGN KEY (PARTICIPANTLIST_ID) REFERENCES participantlist(id)
+);
+
+INSERT INTO participantlist (id) values (1);
