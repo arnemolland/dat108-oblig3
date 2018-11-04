b1bcd15bdc84aada5ce62de58dab054cda2f9298
Arne Olai Molland <arneolaim@gmail.com>
2018-11-04 19:22:20 +0100

    :sparkles: Loads SignupServlet on startup

--- src/main/java/no/hvl/dat108/SignupServlet.java
+++ src/main/java/no/hvl/dat108/SignupServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "SignupServlet", urlPatterns = {"signup"})
+@WebServlet(name = "SignupServlet", urlPatterns = {"signup", "/"}, loadOnStartup = 1)
 public class SignupServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
